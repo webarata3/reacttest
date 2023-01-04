1c0972af8c9cfe1c5c045250bf0f078473436da3
webarata3 <webarata3@gmail.com>
2023-01-04 13:02:54 +0900

    最初のコミット

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Home from './view/Home';
+import { Routes, Route, Link } from 'react-router-dom';
+import About from './view/About';
+import Contact from './view/Contact';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello React Router v6</h1>
+      <ul>
+        <li><Link to="/">home</Link></li>
+        <li><Link to="/about">about</Link></li>
+        <li><Link to="/contact">contact</Link></li>
+      </ul>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 }
